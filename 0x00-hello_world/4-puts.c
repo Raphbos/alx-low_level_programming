c4d38319eee50feebe17e555817f186caf02a62a
Raphbos <unachukwuchiagoziem@gmail.com>
2022-10-20 09:30:50 -0700

    puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* puts - writes out the sentence
-* Return: Always 0 (Success)
-*/
+  * main - Entry point
+  *
+  * puts - writes out the output
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
+
