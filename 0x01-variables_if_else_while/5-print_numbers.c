b40494309393e717c03287522b7d54b169ff27fb
Raphbos <unachukwuchiagoziem@gmail.com>
2022-10-27 09:56:08 -0700

    numbers commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+	int v;
+
+	v = 0;
+
+	while (v <= 9)
+	{
+		printf("%d", v);
+		v++;
+	}
+	printf("\n");
+	return (0);
+}
