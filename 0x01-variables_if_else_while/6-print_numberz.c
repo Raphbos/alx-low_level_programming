91328afed52e9f91aa978a27c423b852f1e9b4bd
Raphbos <unachukwuchiagoziem@gmail.com>
2022-10-27 16:08:32 -0700

    tebahpla commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+	int v;
+
+	for (v = 0; v <= 9; v++)
+	putchar(v);
+	putchar('\n');
+	return (0);
+	}
