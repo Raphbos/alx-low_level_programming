6e330ab667584e2bf6132ef2492a4501524cae6b
Raphbos <unachukwuchiagoziem@gmail.com>
2022-10-27 18:07:21 -0700

    9-print_comb commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int num;
 
-	num = '0';
-
-	do {
-		putchar (num);
-		putchar (',');
-		putchar (' ');
-		num++;
-	} while (num <= '9');
-
+	for (num = 0; num < 10; num++)
+	{
+		putchar (num + '0');
+		if (num < 9)
+		{
+			putchar (',');
+			putchar (' ');
+		}
+	}
 	putchar ('\n');
 	return (0);
 }
