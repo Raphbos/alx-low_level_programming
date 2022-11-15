9e09cb6cb4d42ef96d6287b96bb3f93a01c94101
raphbos <unachukwuchiagoziem@gmail.com>
2022-11-15 10:11:41 +0000

    print alphabets a specific number of times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - prints all the alphabets 10 times
+  * Return: Nothing.
+  */
+void print_alphabet_x10(void)
+{
+	char fish;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		for (fish = 'a'; fish < 'z'; fish++)
+		{
+			_putchar (fish);
+		}
+		i++;
+		_putchar ('\n');
+	}
+}
