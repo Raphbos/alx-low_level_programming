c7316dadc97a9fad88b9470ad873bf11b41d2e15
raphbos <unachukwuchiagoziem@gmail.com>
2022-12-12 18:37:46 +0000

    Times-table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,14 +16,19 @@ void times_table(void)
 		{
 			c = a * b;
 
-			if (c <= 9)
-			{
-				_putchar(c + '0');
-			}
-			else
+			if (c > 9)
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
+			}
+			else if (b == 0)
+			{
+				_putchar(c + '0');
+			}
+			else if (b != 0)
+			{
+				_putchar(' ');
+				_putchar(c + '0');
 			}
 
 			if (b < 9)
