f83c1150ba96ce1eef2dc3de2f37ebaeb64f8af7
raphbos <unachukwuchiagoziem@gmail.com>
2022-12-12 21:53:07 +0000

    Add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * add - a function that adds two intergers and returns the result
+  *
+  * @a: an input interger
+  * @b: an input interger
+  * Return: Returns the result of the addition
+  */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
