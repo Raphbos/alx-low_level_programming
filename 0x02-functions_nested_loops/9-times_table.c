d64dbc26f6713843740919d468b628841dc048c2
raphbos <unachukwuchiagoziem@gmail.com>
2022-12-12 17:19:58 +0000

    Times-table Commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,17 +8,30 @@ void times_table(void)
 {
 	int a;
 	int b;
+	int c;
 
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
-			int c = a * b;
+			c = a * b;
 
-			_putchar((c / 10) + '0');
-			_putchar((c % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
+			if (c <= 9)
+			{
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
